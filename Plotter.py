1a3424437c3b281273d17d8a75468d7afc653bce
Brady Paterson <brady.paterson@ibm.com>
2018-02-23 12:37:26 -0500

    Basic lineplot with python

--- Plotter.py
+++ Plotter.py
@@ -1,16 +1,36 @@
 # https://github.com/dkogan/gnuplotlib
 # Execute with: `Python2 Plotter.py`
+import pandas as pd
 import numpy as np
 import gnuplotlib as gp
 
-x = np.linspace(-5,5,100)
 
-gp.plot( x, np.sin(x) )
+df = pd.read_csv('wpm_ec.csv')
+df.columns = ['race','wpm','Accuracy', 'Rank', 'Racers', 'text_id', 'Timestamp', 'Database', 'Keyboard']
+
+#print df
+
+race = df['race']
+wpm = df['wpm']
+#print race
+#print wpm.values
+print wpm
+
+#df[[Race, wpm]]
 
 
-gp.plot( (x, np.sin(x), {'with': 'boxes'}),
-         (x, np.cos(x), {'legend': 'cosine'}),
 
+x = np.linspace(0, 5, 100, endpoint=False)
+
+
+## Plot syntax:
+#   gp.plot(curve, curve, ...., Plot-and-Curve-options)
+#gp.plot( (x, np.cos(x), {'legend': 'cosine'}),
+
+
+gp.plot( (wpm, {'legend': 'WPM'}),
+
+         title    = 'WPM Graph',
          _with    = 'lines',
          terminal = 'dumb 80,40',
          unset    = 'grid')
