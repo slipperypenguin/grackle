b542943ac5d0e2e8e19a075bc88b7d098284bb11
Brady Paterson <brady.paterson@ibm.com>
2018-02-23 15:37:41 -0500

    Cleanup

--- Plotter.py
+++ Plotter.py
@@ -1,33 +1,20 @@
-# https://github.com/dkogan/gnuplotlib
 # Execute with: `Python2 Plotter.py`
 import pandas as pd
-import numpy as np
+#import numpy as np
 import gnuplotlib as gp
 
-
-df = pd.read_csv('wpm_ec.csv')
+#~/.wpm.csv
+#df = pd.read_csv('wpm_ec.csv')
+df = pd.read_csv('~/.wpm.csv')
 df.columns = ['race','wpm','Accuracy', 'Rank', 'Racers', 'text_id', 'Timestamp', 'Database', 'Keyboard']
 
-#print df
-
-race = df['race']
 wpm = df['wpm']
-#print race
-#print wpm.values
 print wpm
 
-#df[[Race, wpm]]
-
-
-
-x = np.linspace(0, 5, 100, endpoint=False)
-
+#x = np.linspace(0, 5, 100, endpoint=False)
 
 ## Plot syntax:
 #   gp.plot(curve, curve, ...., Plot-and-Curve-options)
-#gp.plot( (x, np.cos(x), {'legend': 'cosine'}),
-#linetype 1 lc rgb "red"
-# set      = 'style line 1 lt rgb "red"',
 
 gp.plot( (wpm, {'legend': 'WPM'}),
 
